1438735c24dd08543ee4621739c88cc8ccb4c311
NghTien2105 <152835405+NghTien2105@users.noreply.github.com>
2026-06-20 09:01:37 +0700

    Add LoginRequest model with properties for account

--- Code/Models/LoginRequest.cs
+++ Code/Models/LoginRequest.cs
@@ -1,1 +1,8 @@
-
+namespace BanhNgotApi.Models
+{
+    public class LoginRequest
+    {
+        public string TaiKhoan { get; set; } = string.Empty;
+        public string MatKhau { get; set; } = string.Empty;
+    }
+}
