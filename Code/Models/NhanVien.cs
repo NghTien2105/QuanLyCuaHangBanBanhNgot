c214a9661a5be51e3a671917ae59116cc4d6a744
NghTien2105 <152835405+NghTien2105@users.noreply.github.com>
2026-06-20 09:03:54 +0700

    Update NhanVien.cs

--- Code/Models/NhanVien.cs
+++ Code/Models/NhanVien.cs
@@ -1,1 +1,15 @@
-
+namespace BanhNgotApi.Models
+{
+    public class NhanVien
+    {
+        public int Id { get; set; }
+        public string MaNV { get; set; } = string.Empty;
+        public string HoTen { get; set; } = string.Empty;
+        public string ChucVu { get; set; } = string.Empty;
+        public string SoDienThoai { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string TaiKhoan { get; set; } = string.Empty;
+        public string MatKhau { get; set; } = string.Empty;
+        public string TrangThai { get; set; } = string.Empty;
+    }
+}
