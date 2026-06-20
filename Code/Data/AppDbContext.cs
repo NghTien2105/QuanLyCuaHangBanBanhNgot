eb59ad202a1c83e613b29f19b7681996c56e6dd0
NghTien2105 <152835405+NghTien2105@users.noreply.github.com>
2026-06-20 08:59:58 +0700

    Update AppDbContext.cs

--- Code/Data/AppDbContext.cs
+++ Code/Data/AppDbContext.cs
@@ -1,1 +1,15 @@
+using BanhNgotApi.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace BanhNgotApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<SanPham> SanPhams { get; set; }
+        public DbSet<NhanVien> NhanViens { get; set; }
+    }
+}
